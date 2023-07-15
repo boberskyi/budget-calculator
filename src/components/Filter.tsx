10f53e73f7f3df65955041ee814262a1a73f1192
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-15 18:21:21 +0200

    Basic skeleton and stlyes of Filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
+import {TbArrowsTransferDown} from "react-icons/tb";
 
 export const Filter = () => {
     return (
-        <div>
-
-        </div>
+        <StyledFilter>
+            <StyledFilterBtn>Group By <TbArrowsTransferDown/></StyledFilterBtn>
+            <StyledFilterBtn>Realisation <TbArrowsTransferDown/></StyledFilterBtn>
+            <StyledFilterBtn>Dates <TbArrowsTransferDown/></StyledFilterBtn>
+            <StyledFilterBtn>Types <TbArrowsTransferDown/></StyledFilterBtn>
+            <StyledFilterBtn>Sample <TbArrowsTransferDown/></StyledFilterBtn>
+            <StyledFilterBtn>Extended <TbArrowsTransferDown/></StyledFilterBtn>
+        </StyledFilter>
     );
 };
+
+const StyledFilter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+const StyledFilterBtn = styled.button`
+  display: inline-flex;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  cursor: pointer;
+  border: 1px solid rgba(128, 128, 128, .4);
+  color: rgba(128, 128, 128, 1);
+`
