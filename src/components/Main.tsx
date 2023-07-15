2171ce47732f477ebe1444ccb02454753d1dab93
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-15 18:20:27 +0200

    Basic styles and skeleton

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {Filter} from "./Filter";
+import styled from "styled-components";
+import {Summary} from "./Summary";
+import {Transactions} from "./Transactions";
 
 export const Main = () => {
     return (
-        <div>
+        <StyledMain>
             <Filter/>
-        </div>
+            <Summary/>
+            <Transactions/>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+  width: 100%;
+  padding-right: 300px;
+  overflow-y: scroll;
+`
